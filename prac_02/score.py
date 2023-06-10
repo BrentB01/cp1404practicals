3ab93a51c49a25a49fda451f93a9b41893c7cf20
Brent <bbarn003@outlook.com>
2023-06-10 12:48:07 +1000

    Convert previous code into function that determines score rating and returns result. Add random score and prints result

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,29 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+from random import randint
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+
+def main():
+
+    score = float(input("Enter score: "))
+    score_rating = determine_score_rating(score)
+    print(score_rating)
+
+    random_score = randint(0, 100)
+    random_result = determine_score_rating(random_score)
+    print(random_score)
+    print(random_result)
+
+def determine_score_rating(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
