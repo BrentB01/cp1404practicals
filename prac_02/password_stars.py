eb75c61dbe32f071d8ceca013cac598be6740243
Brent <bbarn003@outlook.com>
2023-06-10 11:15:42 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,22 @@
-password = len(input("Enter a password: "))
 minimum_password_length = 10
-if password < minimum_password_length:
-    print("Invalid password length.")
-else:
-    print("*" * password)
+
+
+def main():
+    get_password()
+
+
+def get_password():
+    password = len(input("Enter a password: "))
+    is_password_below_minimum_length(password)
+
+
+def is_password_below_minimum_length(password):
+    if password < minimum_password_length:
+        print("Invalid password length.")
+    else:
+        print("*" * password)
+
+
+main()
+
+
